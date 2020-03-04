546e7e6a362da122233a55ff041b7544e4c20f4f
Sharan Narasimhan <sharan.n21@gmail.com>
2020-03-04 15:54:43 +0530

    fixed till too many points to intp

--- undist_from_txt.py
+++ undist_from_txt.py
@@ -1,3 +1,5 @@
+from undistort_sph_im import undistort
+
 import os
 import pandas as pd
 import cv2
@@ -16,7 +18,30 @@ images = []
 
 
 for i in range(len(paths)): 
-    idis = cv2.imread(paths[i])
+
+    idis = cv2.imread('./test_images/'+str(paths[i]))
+    xi = distortion[i]
+    dist = dist + xi
+    (imh, imw, _) = idis.shape
+    f_dist = focal[i] + (imw/imh)*(imh/299)
+    f = f + f_dist
+    u0_dist = imw/2
+    v0_dist = imh/2
+    params_d = {}
+    params_d['f'] = f_dist
+    params_d['W'] = int(u0_dist*2)
+    params_d['H'] = int(v0_dist*2)
+    params_d['xi'] = xi
+
+    params_ud = {}
+    params_ud['f'] = f_dist
+    params_ud['W'] = int(u0_dist*2)
+    params_ud['H'] = int(v0_dist*2)
+
+    image_und = undistort(idis, params_d, params_ud)
+
+
+
 # for i=1:length(paths)
 
 #     Idis = imread(paths{i});
--- undistort_sph_im.py
+++ undistort_sph_im.py
@@ -1,27 +1,31 @@
 import numpy as np
 from math import sqrt
+from scipy import interpolate
 
 
-def undistort(params_d):
-    
-    params_ud = {}
-    params_ud['W'] = params_d['W']*3
-    params_ud['H'] = params_d['H']*3
+def undistort(idis, params_d, params_ud):
+
+
+    # not sure why there was an * 3 in the rhs
+    params_ud['W'] = params_d['W']
+    params_ud['H'] = params_d['H']
 
     # Parameters of the camera to generate
     f_dist = params_d['f']
     u0_dist = params_d['W']/2  
     v0_dist = params_d['H']/2
-    
-    f_undist = Paramsund.f
+
+    f_undist = params_ud['f']
     u0_undist = params_ud['W']/2  
     v0_undist = params_ud['H']/2
-    xi = params_d['x']  
-    [Imd.H, Imd.W, ~] = size(Idis);
+    xi = params_d['xi']
+
+    # (Imd.H, Imd.W, ~] = size(Idis);
 
     # 1. Projection on the image 
     x = np.arange(params_d['W'])
     y = np.arange(params_d['H'])
+
     
     grid_x, grid_y = np.meshgrid(x, y, sparse=True)
     x_cam = np.divide(grid_x, f_undist) - u0_undist/f_undist
@@ -30,32 +34,39 @@ def undistort(params_d):
 
     # 2. Image to sphere cart
     xi1 = 0
-    alpha_cam = (xi1*z_cam + sqrt(z_cam*z_cam + ((1-xi1*xi1)*(x_cam*x_cam + y_cam*y_cam))))/(x_cam*x_cam+y_cam*y_cam+z_cam*z_cam)
+
+    alpha_cam_1 = (xi1*z_cam + np.sqrt(z_cam*z_cam + ((1-xi1*xi1)*(x_cam*x_cam + y_cam*y_cam))))
+    alpha_cam_2 = (x_cam*x_cam+y_cam*y_cam+z_cam*z_cam)
+
+    alpha_cam = alpha_cam_1/alpha_cam_2
 
     x_sph = x_cam * alpha_cam
     y_sph = y_cam * alpha_cam
     z_sph = z_cam * alpha_cam - xi1 
 
     # 3. reprojection on distorted
-    den = xi*(sqrt(x_sph*x_sph + y_sph*y_sph + z_sph*z_sph)) + z_sph 
+    den = xi*(np.sqrt(x_sph*x_sph + y_sph*y_sph + z_sph*z_sph)) + z_sph 
     x_d = ((x_sph*f_dist)/den) + u0_dist
-    y_d = ((y_ph*f_dist)/den) + v0_dist
+    y_d = ((y_sph*f_dist)/den) + v0_dist
  
     # %4. Final step interpolation and mapping
-    img_und = np.zeros((params_ud['H'], params_ud['W'], 3)
-            
-    
-    for c in range(1,3,1):
-        
-    for c=1:3
-    Image_und(:,:,c) = interp2(im2double(Idis(:,:,c)), X_d, Y_d, 'cubic');
-    [Im_und.H, Im_und.W, ~] = size(Image_und);
-    
-    # %ROI
-    min(X_d(:)), max(X_d(:));
-    min(Y_d(:)), max(Y_d(:));
-    size(Idis);
+    img_und = np.zeros((params_ud['H'], params_ud['W'], 3))
 
-if __name__ = "__main__":
-    params_d = {'W':10, 'H':1
+    for c in range(3):
+        image_und[:,:,c] = interpolate.interp2d(idis[:,:,c], x_d, y_d, kind="cubic")
+
+    
+    # for c in range(1,3,1):
+        
+    # for c=1:3
+    # Image_und(:,:,c) = interp2(im2double(Idis(:,:,c)), X_d, Y_d, 'cubic');
+    # [Im_und.H, Im_und.W, ~] = size(Image_und);
+    
+    # # %ROI
+    # min(X_d(:)), max(X_d(:));
+    # min(Y_d(:)), max(Y_d(:));
+    # size(Idis);
+
+# if __name__ = "__main__":
+#     params_d = {'W':10, 'H':1
                 
